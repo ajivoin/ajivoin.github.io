6de3d275e2e7d14d50d02aa05bfe963c01c367af
Andrew Jivoin <andrew@jivoin.com>
2019-12-26 10:22:37 -0500

    temp

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,4 @@
 var experienceClick = function(id) {
   var text = document.getElementById(id).children[1];
-  if (text.classList.contains('hidden')) {
-    text.classList.replace('hidden', 'visible');
-  } else {
-    text.classList.replace('visible', 'hidden');
-  }
+  text.classList.replace('hidden', 'visible') || text.classList.replace('visible', 'hidden');
 }
