6b6e61ed1f0cda2841126cc17919fbc6686ac262
Andrew Jivoin <andrew@jivoin.com>
2020-10-20 23:33:32 -0400

    Adds up-to-date information

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
-var experienceClick = function(id) {
-  var text = document.getElementById(id).children[1];
-  text.classList.replace('hidden', 'visible') || text.classList.replace('visible', 'hidden');
+var transitionLock = new Set();
+var experienceClick = (id) => {
+  if (!transitionLock.has(id)){
+    transitionLock.add(id);
+    var text = document.getElementById(id).children[1];
+    text.classList.replace('hidden', 'visible') || text.classList.replace('visible', 'hidden');
+    transitionLock.delete(id);
+  }
 }
